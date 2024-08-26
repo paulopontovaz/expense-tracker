1f85c2de3fc1117c21c6ad6fa3d494bfc399460d
Paulo Vaz <paulopontovaz@gmail.com>
2024-08-26 20:01:37 +0200

    feat(server): setup routers

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,14 +1,20 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { cacheMiddleware } from "./middlewares/cacheMiddleware";
-import { paymentsRouter } from "./routes";
+import {
+    expenseEntriesRouter,
+    expensePeriodSummariesRouter,
+    participantsRouter,
+    recurrentExpensesRouter,
+} from "./routes";
 
 const server = new Hono().basePath("/api");
 
 if (process.env.NODE_ENV === "dev") {
     server.use(cors());
 }
-server.use("*", cacheMiddleware);
-server.route("/payments", paymentsRouter);
+server.route("/expense-entries", expenseEntriesRouter);
+server.route("/expense-period-summaries", expensePeriodSummariesRouter);
+server.route("/participants", participantsRouter);
+server.route("/recurrent-expenses", recurrentExpensesRouter);
 
 export { server };
