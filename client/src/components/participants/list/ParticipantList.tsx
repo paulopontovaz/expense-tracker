7750b5dd7bc5e0ca3aa8a889c931778db413dce2
Paulo Vaz <paulopontovaz@gmail.com>
2024-10-03 20:34:21 +0200

    refactor(client): update variable name in use of useGetAllParticipants

--- client/src/components/participants/list/ParticipantList.tsx
+++ client/src/components/participants/list/ParticipantList.tsx
@@ -1,17 +1,19 @@
 import { VStack } from "@chakra-ui/react";
+import type { Participant } from "../../../../../server/db/models/schema";
 import { useGetAllParticipants } from "../../../services";
 import { ParticipantListItem } from "./ParticipantListItem";
 
 type ParticipantListProps = {
-    onOpen: () => void;
+    onOpen: (participant?: Participant) => void;
 };
 
 export function ParticipantList(props: ParticipantListProps) {
     const { onOpen } = props;
-    const { participants } = useGetAllParticipants();
+    const { participantList } = useGetAllParticipants();
+
     return (
         <VStack w="full" h="full" spacing={3}>
-            {participants.map((participant) => (
+            {participantList.map((participant) => (
                 <ParticipantListItem
                     key={participant.id}
                     participant={participant}
